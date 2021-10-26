8ffbe525efc07a178ee70681513517d4be95d376
Gio <sethg@ipi.org>
2021-10-26 00:14:51 -0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main">
+    <InteractivePrompt />
+  </div>
+</template>
+
+<script>
+
+import InteractivePrompt from '@/components/InteractivePrompt.vue'
+
+export default {
+  name: 'HelloWorld',
+  components: {InteractivePrompt},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/InteractivePrompt.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="prompt">
+    <span class="textfrag">A game</span>
+    <span class="textfrag">like</span>
+    <div class="radiovert">
+      <label><input type="radio" :value="0" v-model="temp[0]"/> Mario</label>
+      <label><input type="radio" :value="1" v-model="temp[0]"/> Luigi</label>
+    </div>
+    <span class="textfrag">that's about</span>
+    <span class="textfrag">with an engine like</span>
+    <span class="textfrag">set in a</span>
+    <span class="textfrag">theme</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'InteractivePrompt',
+  data () {
+    return {
+      temp: [],
+      wordlists: {
+        game: [
+          "Mario"
+        ]
+      }
+    }
+  }
+}
+</script>
+s
+<style scoped lang="scss">
+
+.radiovert {
+  display: inline-block;
+  label {
+    display: block;
+  }
+}
+</style>
